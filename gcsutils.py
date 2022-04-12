a99f646acaa52c553041cbf631973c5dcb86ab0e
Kohsaku Nishida <kohsaku.nishida@gmail.com>
2022-04-12 21:11:57 +0900

    命名規約の変更とsetup.pyの追加

--- gcsutils.py
+++ gcsutils.py
@@ -6,77 +6,75 @@ from datetime import datetime, timedelta, timezone
 
 from pathlib import Path
 from typing import Iterable, List
-
-class GCS_utils:
-   def __init__(self, project_id :str, bucket_name: str, work_name: str, result_path: str, weight_path: str) -> None:
-       auth.authenticate_user()
-       self.storage_client = storage.Client(project= project_id)
-       self.bucket = self.storage_client.get_bucket(bucket_name)
-       self._work_name = work_name
-       self._result_path = result_path
-       self._weight_path = weight_path
+import subprocess
 
 
-   def download(self, gcs_dl_path: str, local_name: str, exist_ok = False, unzip_flag = False):
-       if not os.path.exists(local_name) or exist_ok:
+class GcsUtils:
+
+    def __init__(self, project_id: str, bucket_name: str, work_name: str, result_path: str, weight_path: str) -> None:
+        auth.authenticate_user()
+        self.storage_client = storage.Client(project=project_id)
+        self.bucket = self.storage_client.get_bucket(bucket_name)
+        self._work_name = work_name
+        self._result_path = result_path
+        self._weight_path = weight_path
+
+    def download(self, gcs_dl_path: str, local_name: str, exist_ok=False, unzip_flag=False) -> None:
+        if not os.path.exists(local_name) or exist_ok:
             blob = self.bucket.blob(gcs_dl_path)
             blob.download_to_filename(local_name)
             print(f"{local_name}をダウンロードしました")
-            if(unzip_flag):
+            if unzip_flag:
                 with zipfile.ZipFile(local_name) as zf:
-                     zf.extractall()
+                    zf.extractall()
 
+    def upload(self, local_name: str, gcs_ul_path: str):
+        blob = self.bucket.blob(gcs_ul_path)
+        blob.upload_from_filename(local_name)
+        print(f"{local_name}をアップロードしました")
 
-   def upload(self, local_name: str, gcs_ul_path: str):
-       blob = self.bucket.blob(gcs_ul_path)
-       blob.upload_from_filename(local_name)
-       print(f"{local_name}をアップロードしました")
+    def zip_upload(self, local_name: str, gcs_ul_path: str, zip_name: str):
+        zf = zipfile.ZipFile(zip_name + ".zip", 'w')
+        zf.write(local_name, zip_name, compress_type=zipfile.ZIP_DEFLATED)
+        zf.close()
+        self.upload(gcs_ul_path, zip_name + ".zip")
 
+    def output_upload(self, result_dir, model_name, check_point_file, learning_rate, max_itr):
+        if os.path.exists(check_point_file):
+            os.remove(check_point_file)
 
-   def zip_upload(self, local_name: str, gcs_ul_path: str, zip_name: str):
-       zf = zipfile.ZipFile(zip_name + ".zip", 'w')
-       zf.write(local_name, zip_name, compress_type = zipfile.ZIP_DEFLATED)
-       zf.close()
-       self.upload(gcs_ul_path, zip_name + ".zip")
-   
+        _results_file = self.__prepare_upload(learning_rate, max_itr)
 
-   def output_upload(self, result_dir, model_name, check_point_file, learning_rate, max_itr):
-       if os.path.exists(check_point_file):
-          os.remove(check_point_file)
+        # 重みファイル
+        self._weight_name = _results_file + '.pth'
 
-       _results_file = self.__prepare_upload(learning_rate, max_itr)
+        # outputフォルダのzip
+        self._result_file_zip = _results_file + '.zip'
 
-       # 重みファイル
-       self._weight_name = _results_file + '.pth'
+        if os.path.exists(model_name):
+            self.upload(model_name, os.path.join(self.weight_path, self._weight_name))
 
-       # outputフォルダのzip
-       self._result_file_zip = _results_file + '.zip'
+        self.__compress_dir(Path(self._result_file_zip), Path(result_dir), ".pth")
 
-       if os.path.exists(model_name):
-          self.upload(model_name, os.path.join(self.weight_path, self._weight_name))
+        self.upload(self._result_file_zip, os.path.join(self.result_path, self._result_file_zip))
 
-       self.__compress_dir(Path(self._result_file_zip), Path(result_dir), ".pth")
+    def __prepare_upload(self, learning_rate, max_itr):
+        # タイムゾーンの生成
+        JST = timezone(timedelta(hours=+9), 'JST')
+        tdatetime = datetime.now(JST)
+        date_stirngs = tdatetime.strftime('%Y%m%d')
+        time_strings = tdatetime.strftime('%H%M')
+        results_file = date_stirngs + '_' + time_strings + '_' + "results_train" + '_' + self.work_name + '_' + f"{learning_rate:.0e}" + '_' + str(
+            max_itr)
 
-       self.upload(self._result_file_zip, os.path.join(self.result_path, self._result_file_zip))
+        return results_file
 
-
-   def __prepare_upload(self, learning_rate, max_itr):
-       # タイムゾーンの生成
-       JST = timezone(timedelta(hours=+9), 'JST')
-       tdatetime = datetime.now(JST)
-       date_stirngs = tdatetime.strftime('%Y%m%d')
-       time_strings = tdatetime.strftime('%H%M')
-       results_file = date_stirngs + '_' + time_strings + '_' + "results_train" + '_' + self.work_name + '_' + f"{learning_rate:.0e}" + '_' + str(max_itr)
-
-       return results_file
-
-
-   def __compress_dir(self, zip_out: Path, directory: Path, skipped_names: Iterable[str]):
-       # ファイル圧縮
-       settings = {
-          'verbose': False,
-       }
-       with zipfile.ZipFile(zip_out.absolute(), 'w') as z:
+    def __compress_dir(self, zip_out: Path, directory: Path, skipped_names: Iterable[str]):
+        # ファイル圧縮
+        settings = {
+            'verbose': False,
+        }
+        with zipfile.ZipFile(zip_out.absolute(), 'w') as z:
             if settings['verbose']:
                 print('Zip file has been initialized: "{}".'.format(z.filename))
             for f in self.__iter_all_files(directory):
@@ -90,28 +88,47 @@ class GCS_utils:
                 if settings['verbose']:
                     print('Added: "{}".'.format(f))
 
-   def __iter_all_files(self, directory: Path) -> List[Path]:
-      '''指定されたディレクトリの下にあるすべてのファイル（ Path ）を下層のものも含めて返すイテレータを返す
-      '''
-      if not directory.is_dir():
-          message = 'Specified directory is not found: "{}".'.format(directory)
-          raise self.CompressionError(message)
+    def __iter_all_files(self, directory: Path) -> List[Path]:
+        """指定されたディレクトリの下にあるすべてのファイル（ Path ）を下層のものも含めて返すイテレータを返す
+        """
+        if not directory.is_dir():
+            message = 'Specified directory is not found: "{}".'.format(directory)
+            raise self.CompressionError(message)
 
-      return directory.glob('**/*.*')
-  
-   class CompressionError(Exception):
-      '''zip 圧縮プロセスで発生するエラーを表すカスタム例外
-      '''
-      pass
+        return directory.glob('**/*.*')
 
-   @property
-   def work_name(self):
-       return self._work_name
+    class CompressionError(Exception):
+        """zip 圧縮プロセスで発生するエラーを表すカスタム例外
+        """
+        pass
 
-   @property
-   def result_path(self):
-       return self._result_path
+    @property
+    def work_name(self):
+        return self._work_name
 
-   @property
-   def weight_path(self):
-       return self._weight_path
+    @property
+    def result_path(self):
+        return self._result_path
+
+    @property
+    def weight_path(self):
+        return self._weight_path
+
+
+def download_keys(gcsutils: GcsUtils) -> None:
+    """
+    tor key file
+    :param gcsutils: GcsUtils instance
+    :return: None
+    """
+    keyfile = 'bulkyad_key.zip'
+    gcsutils.download(keyfile, keyfile, exist_ok=True, unzip_flag=True)
+
+    subprocess.run('mkdir -p /root/.ssh', shell=True,
+                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    subprocess.run('mv id_rsa /root/.ssh', shell=True,
+                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    subprocess.run('ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts', shell=True,
+                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    subprocess.run('chmod go-rwx /root/.ssh/id_rsa', shell=True,
+                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colabutils',
+    version='0.1.0',
+    description='Bulky Anomaly Detector Colabo Utilities',
+
+    author='Bulky Info TEC INC.',
+    author_email='kyosuke.matsumoto@bulky.co.jp',
+    url='www.bulky.co.jp',
+
+    packages=find_packages(),
+)
