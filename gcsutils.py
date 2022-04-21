3b4fb9cca8f66101c63164ed987b843db732c99a
matsumotoBULKY <74278810+matsumotoBULKY@users.noreply.github.com>
2022-04-21 16:26:28 +0900

    Update gcsutils.py
    Add comment

--- gcsutils.py
+++ gcsutils.py
@@ -12,6 +12,20 @@ import subprocess
 class GcsUtils:
 
     def __init__(self, project_id: str, bucket_name: str, work_name: str, result_path: str, weight_path: str) -> None:
+        """
+        <init>
+        Args:
+            project_id str:
+                Google Cloud Storageのプロジェクト名
+            bucket_name str:
+                Google Cloud Storageのバケット名
+            work_name str:
+                検出対象の製品名
+            result_path str:
+                Google Cloud Storageの結果を保存する場所のパス
+            weight_path str:
+                Google Cloud Storageの重みを保存する場所のパス
+        """
         auth.authenticate_user()
         self.storage_client = storage.Client(project=project_id)
         self.bucket = self.storage_client.get_bucket(bucket_name)
@@ -20,6 +34,19 @@ class GcsUtils:
         self._weight_path = weight_path
 
     def download(self, gcs_dl_path: str, local_name: str, exist_ok=False, unzip_flag=False) -> None:
+        """
+        <download>
+            GCSの指定したパスから指定したファイルをローカルにダウンロードする。
+        Args:
+            gcs_dl_path str:
+                Google Cloud Storageのダウンロードパス
+            local_name str:
+                ローカルでのファイル名
+            exist_ok boolean:
+                同じファイル名が存在する場合上書きを行うか、False=重複禁止、True=上書き
+            unzip_flag boolean:
+                zipファイルを解凍するか、False=解凍しない、zipファイルではない、True=zipファイルを解凍する
+        """
         if not os.path.exists(local_name) or exist_ok:
             blob = self.bucket.blob(gcs_dl_path)
             blob.download_to_filename(local_name)
@@ -29,17 +56,52 @@ class GcsUtils:
                     zf.extractall()
 
     def upload(self, local_name: str, gcs_ul_path: str):
+        """
+        <upload>
+            指定したファイルをGCSの指定したパスにアップロードする。
+        Args:
+            local_name str:
+                ローカルでのファイル名
+            gcs_ul_path str:
+                Google Cloud Storageのアップロードパス
+        """
         blob = self.bucket.blob(gcs_ul_path)
         blob.upload_from_filename(local_name)
         print(f"{local_name}をアップロードしました")
 
     def zip_upload(self, local_name: str, gcs_ul_path: str, zip_name: str):
+        """
+        <zip_upload>
+            指定したファイルを圧縮した後にGCSの指定したパスにアップロードする。
+        Args:
+            local_name str:
+                ローカルでのファイル名
+            gcs_ul_path str:
+                Google Cloud Storageのアップロードパス
+            zip_name str:
+                圧縮したあとのファイル名
+        """
         zf = zipfile.ZipFile(zip_name + ".zip", 'w')
         zf.write(local_name, zip_name, compress_type=zipfile.ZIP_DEFLATED)
         zf.close()
         self.upload(zip_name + ".zip", gcs_ul_path)
 
     def output_upload(self, result_dir, model_name, check_point_file, learning_rate, max_itr):
+        """
+        <output_upload>
+            学習結果をGCSの指定したパスにアップロードする。
+        Args:
+            result_dir str:
+                学習結果が格納されたローカルのディレクトリ
+            model_name str:
+                最終保存された重みファイル
+            check_point_file str:
+                途中保存された重みファイル
+            learning_rate double:
+                学習率
+            max_itr int:
+                学習回数
+        """
         if os.path.exists(check_point_file):
             os.remove(check_point_file)
 
@@ -59,6 +121,18 @@ class GcsUtils:
         self.upload(self._result_file_zip, os.path.join(self.result_path, self._result_file_zip))
 
     def __train_prepare_upload(self, learning_rate, max_itr):
+        """
+        <__train_prepare_upload>
+            学習結果のアップロードする際に命名する文字列を生成し、返す。
+        Args:
+            learning_rate double:
+                学習率
+            max_itr int:
+                イテレーション数
+        Return:
+            results_file str:
+                日付、時間、ワーク名、学習率、学習回数を組み合わせた文字列
+        """
         # タイムゾーンの生成
         JST = timezone(timedelta(hours=+9), 'JST')
         tdatetime = datetime.now(JST)
@@ -69,7 +143,15 @@ class GcsUtils:
         return results_file
 
     def inference_upload(self, result_dir, thresh_hold):
-
+        """
+        <inference_upload>
+            推論結果をGCSの指定したパスにアップロードする。
+        Args:
+            result_dir str:
+                推論結果が格納されたローカルのディレクトリ
+            thresh_hold double:
+                異常の閾値
+        """
         _results_file = self.__inference_prepare_upload(thresh_hold)
 
         # predict_images フォルダのzip
@@ -78,6 +160,16 @@ class GcsUtils:
         self.zip_upload(result_dir, os.path.join(self.result_path, self._result_file_zip), _results_file)
 
     def __inference_prepare_upload(self, thresh_hold):
+        """
+        <__inference_prepare_upload>
+            学習結果のアップロードする際に命名する文字列を生成し、返す。
+        Args:
+            thresh_hold double:
+                異常の閾値
+        Return:
+            results_file str:
+                日付、時間、ワーク名、閾値を組み合わせた文字列
+        """
         # タイムゾーンの生成
         JST = timezone(timedelta(hours=+9), 'JST')
         tdatetime = datetime.now(JST)
@@ -88,6 +180,17 @@ class GcsUtils:
         return results_file
 
     def __compress_dir(self, zip_out: Path, directory: Path, skipped_names: Iterable[str]):
+         """
+        <__compress_dir>
+            指定されたファイルをzip形式で圧縮する。
+        Args:
+            zip_out Path:
+                圧縮後の名前を含むパス
+            directory Path:
+                圧縮対象ディレクトリ
+            skipped_name Iterable[str]:
+                圧縮対象ディレクトリ内のファイルから指定した文字列を含むものを除いて圧縮
+        """
         # ファイル圧縮
         settings = {
             'verbose': False,
@@ -107,8 +210,17 @@ class GcsUtils:
                     print('Added: "{}".'.format(f))
 
     def __iter_all_files(self, directory: Path) -> List[Path]:
-        """指定されたディレクトリの下にあるすべてのファイル（ Path ）を下層のものも含めて返すイテレータを返す
+         """
+        <__iter_all_files>
+            指定されたディレクトリの下にあるすべてのファイル（ Path ）を下層のものも含めて返すイテレータを返す。
+        Args:
+            directory Path:
+                検索対象のディレクトリ
+        Return:
+            directory.glob('**/*.*')
+                ディレクトリ内のすべてのファイル
         """
+  
         if not directory.is_dir():
             message = 'Specified directory is not found: "{}".'.format(directory)
             raise self.CompressionError(message)
@@ -116,7 +228,8 @@ class GcsUtils:
         return directory.glob('**/*.*')
 
     class CompressionError(Exception):
-        """zip 圧縮プロセスで発生するエラーを表すカスタム例外
+        """
+        zip 圧縮プロセスで発生するエラーを表すカスタム例外
         """
         pass
 
